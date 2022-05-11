f45068d9608c9045f20606df6f846520e439ea61
hej <jingxuan.he@inf.ethz.ch>
2022-05-11 15:22:05 +0200

    preliminary evaluation: fix 12.sol

---
+++ project/preliminary_test_contracts/12-1.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+// the contract is vulnerable
+// the output of your analyzer should be Tainted
+contract Contract {
+  function foo(int i) public {
+    address x;
+    if (i > 5) {
+      x = address(0xDEADBEEF);
+    }
+    require(msg.sender == x); // not guard
+    selfdestruct(msg.sender); // vulnerable
+  }
+}
--- project/preliminary_test_contracts/12.sol
+++ project/preliminary_test_contracts/12.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.5.0;
 
-// the contract is vulnerable
-// the output of your analyzer should be Tainted
+// the contract is safe
+// the output of your analyzer should be Safe
 contract Contract {
   function foo(int i) public {
     address x;
     if (i > 5) {
       x = address(0xDEADBEEF);
-      require(msg.sender == x); // not a guard
-      selfdestruct(msg.sender); // vulnerable
+      require(msg.sender == x); // guard
+      selfdestruct(msg.sender); // safe. execution of the if branch stops here
     }
   }
 }
