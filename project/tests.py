a3eba94700b558579bb23741a675ad5a27b674c1
Lukas <lukfried@protonmail.ch>
2022-05-17 22:50:55 +0200

    added Test Rig

---
+++ project/tests.py
@@ -0,0 +1,80 @@
+import os
+import subprocess
+
+CONTRACTS_PATH = './project/test_contracts'
+
+
+
+
+def create_solutions():
+    # filename -> [True=vulnerable contract | False=safe contract]
+    vulnerable = {}
+
+    # get user input for each file
+    for f in os.listdir(CONTRACTS_PATH):
+        print(f"{10*'-'}  {f}  {10*'-'}")
+        with open(f'{CONTRACTS_PATH}/{f}', 'r') as file:
+            content = file.read()            
+            print(content)
+
+            dec = ''
+            while dec not in ['s', 'v', 'skip']:
+                dec = input("Decision: ")
+            
+            if dec == 's':
+                vulnerable[f] = False
+            elif dec == 'v':
+                vulnerable[f] = True
+            else:
+                print(f"Skipped file {f}.")
+
+    # safe to file
+    with open('./tests_solutions','w') as file:
+        for k, v in vulnerable.items():
+            file.write(f"{k},{v}\n")
+    return
+
+
+def get_solutions():
+    with open('./tests_solutions', 'r') as file:
+        content = file.read().rstrip()
+        conv = lambda b: 'Tainted' if b else 'Safe'
+        vulnerable = {e[0]: conv(e[1]=='True') for e in [l.split(',') for l in content.split('\n')]}
+        return vulnerable
+
+
+def run_tests():
+    
+    solutions = get_solutions()
+    score = 0
+    no_passed = 0
+
+    for f in solutions.keys():
+        result  = subprocess.run(['python', './analyze.py', f'./test_contracts/{f}'], stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
+
+        # Test passed 1
+        if solutions[f]  == result:
+            print(f"\033[92mPASSED:\033[0m {f}")
+            score += 1
+            no_passed += 1
+            pass
+        else:
+            # Says Tainted, is Safe 0
+            if result == 'Tainted':
+                color = '\033[93m'
+            # Says Safe, is Tainted -2
+            else:
+                color = '\033[91m'
+                score -= 2
+
+            print(f"{color}FAILED:\033[0m {f} expected: {solutions[f]}, got: {result}")
+
+    print(f"Passed ({no_passed}/{len(solutions)}) with score ({score}/{len(solutions)})")
+
+    return
+
+if __name__ == "__main__":
+    # create_solutions()
+    # get_solutions()
+    run_tests()
+
